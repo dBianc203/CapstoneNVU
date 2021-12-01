11bc77dbe872a3a33276b43fcae078ef22f2272b
Devon Biancarelli <62315100+dBianc203@users.noreply.github.com>
2021-12-01 00:25:55 -0500

    Update BMI.js

--- Jscript/BMI.js
+++ Jscript/BMI.js
@@ -11,5 +11,17 @@ function Calculate(){
     //if the varible "result" is NOT not a number, display the result inside "bmi-output"
     if(!isNaN(result)){
         document.getElementById("bmi-output").innerHTML = result;
+        if(result < 18.5){
+            document.getElementById("bmi-status").innerHTML = "Underweight";
+        }
+        else if(result < 25){
+            document.getElementById("bmi-status").innerHTML = "Healthy";
+        }
+        else if(result < 30){
+            document.getElementById("bmi-status").innerHTML = "Overweight";
+        }
+        else{
+            document.getElementById("bmi-status").innerHTML = "Obesity";
+        }
 }
 }
