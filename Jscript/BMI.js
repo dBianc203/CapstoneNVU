9faa9841ce16f5cc7bd3940c0aa701c7c40578f3
Devon Biancarelli <62315100+dBianc203@users.noreply.github.com>
2021-12-01 00:23:03 -0500

    updated comments

--- Jscript/BMI.js
+++ Jscript/BMI.js
@@ -1,8 +1,14 @@
+//function Calculate 
 function Calculate(){
+    //Variable height takes the element that is labeled "Height" in the html file
     var height = document.getElementById("Height").value;
+    //variable weight take the element that is labeled "Weight" in the html file
     var weight = document.getElementById("Weight").value;
 
+    //varible result take the weight, divides that by the height and weight together multiplied by 703
     var result = (parseFloat(weight) / parseFloat(height)/parseFloat(height))*703;
+    //isNAN = is not a number
+    //if the varible "result" is NOT not a number, display the result inside "bmi-output"
     if(!isNaN(result)){
         document.getElementById("bmi-output").innerHTML = result;
 }
