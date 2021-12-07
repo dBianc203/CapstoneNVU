272daa7e52606b3d02e0780b9f14f45dafa311bd
Devon Biancarelli <62315100+dBianc203@users.noreply.github.com>
2021-12-06 19:15:53 -0500

    Update welcome.php

--- PHP/welcome.php
+++ PHP/welcome.php
@@ -12,7 +12,6 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
     <title>Welcome</title>
     <style>
         body
@@ -25,8 +24,6 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <body>
     <h1 class="my-5">Thank you for logging in </h1>
     <p>
-        <!--<a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>-->
-        <!-- change when working with php to index.php for session -->
         <a href="index.html">Click here to continue to the website</a> <br>
         <a href="logout.php">Sign Out of Your Account</a>
     </p>
