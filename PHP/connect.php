0d67d4aeb24881eb62c691532f5bb7e60bb05bc3
Devon Biancarelli <62315100+dBianc203@users.noreply.github.com>
2021-12-09 03:15:42 -0500

    Update connect.php

--- PHP/connect.php
+++ PHP/connect.php
@@ -6,10 +6,13 @@ define('DB_PASSWORD', '7#i%VzgOFTd0E!UQ');
 define('DB_NAME', 'id17908684_loginsystem');
  
 /* Attempt to connect to MySQL database */
+/* referenced CIS4520 connect.php file, I remember having some issues with the original php file, so i decided to define
+the names and connect with $link rather than $conn to avoid naming issues like last time */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
 // Check connection displays if connection is made
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+//before i would just have "echo "databsed conn failed" " but wanted to use "die" over an echo to exit the script
+if(!$link){
+    die("ERROR: Could not connect.");
 }
 ?>
