45cb147b0537cabdb1d5e8d63a25eb3cdda7a57a
Devon Biancarelli <62315100+dBianc203@users.noreply.github.com>
2021-12-11 02:49:00 -0500

    Update welcome.php

--- PHP/welcome.php
+++ PHP/welcome.php
@@ -2,15 +2,16 @@
 // Initialize the session
 session_start();
 
-// Check if the user is logged in, if not then redirect him to login page
+// Check if the user is logged in, if not then redirect them to the login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
 }
 ?>
 
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <title>Welcome</title>
     <style>
@@ -22,9 +23,10 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     </style>
 </head>
 <body>
-    <h1 class="my-5">Thank you for logging in </h1>
+    <h1>Thank you for logging in </h1>
     <p>
-        <a href="index.html">Click here to continue to the website</a> <br>
+        <!-- change when working with php to index.php for session -->
+        <a href="\">Click here to continue to the website</a> <br>
         <a href="logout.php">Sign Out of Your Account</a>
     </p>
 </body>
